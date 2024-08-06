0c2154f7211fb3c263cf4ccf512a0345d67654c8
Elizabeth Jimenez <elizabeth.jimenez.cs@gmail.com>
2024-08-06 13:48:56 -0400

    Counting Sort 1 Complete

--- countingSort1.js
+++ countingSort1.js
@@ -1,8 +1,14 @@
-function countingSort1(a) {
+function countingSort1(arr) {
+    let frequencyArr = Array(100).fill(0);
 
-    return ;
+    arr.forEach(aa => {
+        frequencyArr[aa] = ++frequencyArr[aa];
+    })
+
+    // return int[100] frequency array
+    return frequencyArr;
 }
 
-const input1 = [];
+const input1 = [1, 1, 3, 2, 1]; // should return [0, 3, 1, 1]
 
 console.log(countingSort1(input1));
